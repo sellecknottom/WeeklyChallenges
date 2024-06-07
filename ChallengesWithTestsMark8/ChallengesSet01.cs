9d182fd33dc78461a65c1d0f0aed59fb2bfa6bd2
Selleck Elliott <selleck.elliott@gmail.com>
2024-06-07 13:29:16 -0600

    Weekly Challenge 1

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,45 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 > number2)
+            {
+                return number2;
+            }
+            else
+            {
+                return number1;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Enter your name:");
+            nameOfPerson = Console.ReadLine();
+            Console.Clear();
+            return $"Hello {nameOfPerson}";
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Enter your name:");
+            string nameOfPerson = Console.ReadLine();
+            Console.Clear();
+            return $"HEY! {nameOfPerson}";
         }
     }
 }
